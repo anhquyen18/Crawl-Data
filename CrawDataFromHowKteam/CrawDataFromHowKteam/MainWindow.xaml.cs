bc56356ca6bedca4b4e70f5753abe7bc1d51033c
anhquyen18 <anhquyen18092000@gmail.com>
2022-08-02 22:00:29 +0700

    crawl data 30%

--- CrawDataFromHowKteam/CrawDataFromHowKteam/MainWindow.xaml.cs
+++ CrawDataFromHowKteam/CrawDataFromHowKteam/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,11 +25,21 @@ namespace CrawDataFromHowKteam
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region properties
+        string homePage = "https://howkteam.vn/";
+        HttpClient httpClient = new HttpClient();
+        ObservableCollection<MenuTreeItem> TreeItems = new ObservableCollection<MenuTreeItem>();
+        HttpClientHandler handler;
+        CookieContainer cookie = new CookieContainer();
+
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
 
-            ObservableCollection<MenuTreeItem> TreeItems = new ObservableCollection<MenuTreeItem>();
+
+            IniHttpClient();
+
             MenuTreeItem item1 = new MenuTreeItem()
             {
                 Name = "Item1",
@@ -66,10 +80,55 @@ namespace CrawDataFromHowKteam
         {
             root.Add(node);
         }
+
+        void IniHttpClient()
+        {
+            handler = new HttpClientHandler
+            {
+                CookieContainer = cookie,
+                ClientCertificateOptions = ClientCertificateOption.Automatic,
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                AllowAutoRedirect = true,
+                UseDefaultCredentials = false
+            };
+
+
+            //httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77");
+
+            httpClient = new HttpClient(handler);
+            httpClient.BaseAddress = new Uri(homePage);
+        }
+
+        string CrawlDataFromURL(string url)
+        {
+            string html = "";
+
+
+            html = httpClient.GetStringAsync(url).Result;
+                
+            return html;
+        }
+
+        void Crawl(string url)
+        {
+            string htmlLearn = CrawlDataFromURL(url);
+
+            string courseListKey = "<div class=\"col-md-6 col-lg-4 col-xl-3\">(.*?)</div>";
+            var CourseList = Regex.Matches(htmlLearn, courseListKey, RegexOptions.Singleline);
+            foreach (var course in CourseList)
+            {
+                //string courseName = Regex.Match(course.ToString)
+            }
+        }
         #endregion
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Crawl("learn");
         }
     }
 }
